43cfb0d5725afa5081cb4543ecda951ecd63d829
Clemens Ibrom <clemens.ibrom@stud.uni-goettingen.de>
2026-08-14 11:38:45 +0200

    initial commit

---
+++ src/main/java/org/javalearning/tennisdash/TennisDashApplication.java
@@ -0,0 +1,13 @@
+package org.javalearning.tennisdash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TennisDashApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TennisDashApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/javalearning/tennisdash/TennisDashApplicationTests.java
@@ -0,0 +1,13 @@
+package org.javalearning.tennisdash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TennisDashApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
