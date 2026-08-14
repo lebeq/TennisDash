43a0ab003201810c85538d9736ffee04021021aa
Clemens Ibrom <clemens.ibrom@stud.uni-goettingen.de>
2026-08-14 12:48:01 +0200

    added web controller and tests for controller and integration

--- src/main/java/org/javalearning/tennisdash/TennisDashApplication.java
+++ src/main/java/org/javalearning/tennisdash/TennisDashApplication.java
@@ -1,13 +1,34 @@
 package org.javalearning.tennisdash;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+import java.util.Arrays;
+
+//once DB is added, remove the (...) after SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class TennisDashApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(TennisDashApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+            System.out.println("Let's inspect da beanz provided by Spring boot:");
+
+            String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            for (String beanName: beanNames){
+                System.out.println(beanName);
+            }
+        };
     }
 
 }
--- src/test/java/org/javalearning/tennisdash/TennisDashApplicationTests.java
+++ src/test/java/org/javalearning/tennisdash/TennisDashApplicationTests.java
@@ -3,11 +3,26 @@ package org.javalearning.tennisdash;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
 @SpringBootTest
+@AutoConfigureMockMvc
 class TennisDashApplicationTests {
 
+    @Autowired
+    private MockMvc mvc;
+
     @Test
-    void contextLoads() {
+    public void getHello() throws Exception {
+        mvc.perform(get("/").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk()).andExpect(content().string(equalTo("Oy Oy!")));
     }
 
 }
